fb44e87e751d08a9bcf138e00e7d8c80e84117c9
RaVadim <rasstriginv@gmail.com>
2020-04-29 20:21:12 +0400

    without dist

--- dist/js/chunk-2d217357.81b5f6ec.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217357"],{c66d:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an PROFILE page")])])}],c=t("2877"),u={},i=Object(c["a"])(u,s,a,!1,null,null,null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d217357.81b5f6ec.js.map
